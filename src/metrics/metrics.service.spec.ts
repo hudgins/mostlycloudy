00d834d029fcc435752afb9eae8b2a5b14de997a
Allan Hudgins <al.hudgins@gmail.com>
2024-02-27 16:38:21 -0800

    test: adds missing tests for metrics service

--- src/metrics/metrics.service.spec.ts
+++ src/metrics/metrics.service.spec.ts
@@ -15,4 +15,16 @@ describe('MetricsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should report a metric and retrieve it', () => {
+    service.incrementMetric('test.metric');
+    service.incrementMetric('test.metric');
+    expect(service.getMetric('test.metric')).toEqual(2);
+  });
+
+  it('should report a metric by providing an increment amount and retrieve it', () => {
+    service.incrementMetric('test.metric', 5);
+    service.incrementMetric('test.metric', 10);
+    expect(service.getMetric('test.metric')).toEqual(15);
+  });
 });
